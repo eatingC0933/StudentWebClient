cdd1f92c3511f39cc30654ef27c8b7eba2bf292b
Yiting Chang <yiting.chang@iths.se>
2025-05-08 12:39:17 +0200

    Update StudentController.java

--- src/main/java/se/yitingchang/studentwebclient/StudentController.java
+++ src/main/java/se/yitingchang/studentwebclient/StudentController.java
@@ -1,4 +1,38 @@
 package se.yitingchang.studentwebclient;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/students")
+@CrossOrigin
 public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Student> getAll() {
+        return service.getAll();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return service.add(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable int id, @RequestBody Student student) {
+        return service.update(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable int id) {
+        service.delete(id);
+    }
 }
+
