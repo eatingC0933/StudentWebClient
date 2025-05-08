be1b7c4cf334f24ef6b45338a862a7aec82d6ce9
Yiting Chang <yiting.chang@iths.se>
2025-05-08 12:40:01 +0200

    Update StudentService.java

--- src/main/java/se/yitingchang/studentwebclient/StudentService.java
+++ src/main/java/se/yitingchang/studentwebclient/StudentService.java
@@ -1,4 +1,44 @@
 package se.yitingchang.studentwebclient;
 
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class StudentService {
+
+    private final StudentRepository repository;
+
+    public StudentService(StudentRepository repository) {
+        this.repository = repository;
+    }
+
+
+    public List<Student> getAll() {
+        return repository.findAll();
+    }
+
+
+    public Student add(Student student) {
+        return repository.save(student);
+    }
+
+
+    public Student update(int id, Student student) {
+        Optional<Student> existing = repository.findById(id);
+        if (existing.isPresent()) {
+            student.setId(id);
+            return repository.save(student);
+        }
+        throw new EntityNotFoundException("Student with id " + id + " not found.");
+    }
+
+
+    public void delete(int id) {
+        repository.deleteById(id);
+    }
 }
+
